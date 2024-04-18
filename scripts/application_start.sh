568ed335b3b9a17ec032175be266820e901b5e44
Mai Van Manh <mvmanh@gmail.com>
2024-04-18 22:48:05 +0700

    fix

---
+++ scripts/application_start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Example script to start the application after installation
+# Replace the following lines with commands specific to your application
+
+echo "Starting the application..."
+cd /opt/myapp
+java -jar app-0.0.1-SNAPSHOT.jar &
---
+++ scripts/before_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Example script to stop any existing services before installing the new version of the application
+# Replace the following lines with commands specific to your application
+
+echo "Stopping existing services..."
+# sudo systemctl stop my-application.service || true
